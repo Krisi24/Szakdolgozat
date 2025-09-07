d17566488bfbdf8824bded313f24fcb58161f950
CsKristof <99207284+Krisi24@users.noreply.github.com>
2025-09-07 16:31:04 +0200

    Add Watcher enemy and Detection

--- Assets/Actors/Enemy/EnemyTypes/Watch/Watch.cs
+++ Assets/Actors/Enemy/EnemyTypes/Watch/Watch.cs
@@ -2,15 +2,130 @@ using UnityEngine;
 
 public class Watch : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float orbitSpeed = 0.5f; // Sebesség, amellyel a spotlámpa kering
+    public float xAxisRadius = 5f; // Az ellipszis X tengely sugara
+    public float yAxisRadius = 3f; // Az ellipszis Y tengely sugara
+
+    [Header("Detektáló gömb beállításai")]
+    public float detectionSphereRadius = 1f; // A detektáló gömb sugara
+    public float detectionSphereDistance = 2f; // A detektáló gömb távolsága a spotlámpától az elõre irányban
+    public LayerMask hitLayers; // Választható layerek az OverlapSphere számára
+
+    private Light spotLight;
+
+    // Változók a Sphere Gizmo-hoz
+    private Vector3 gizmoSphereCenter;
+    private float gizmoSphereRadius;
+    private Collider[] detectedColliders; // Tárolja a detektált collider-eket
+    private bool gizmoSphereDetected;
+
     void Start()
     {
-        
+        spotLight = GetComponentInChildren<Light>();
+
+        if (spotLight == null || spotLight.type != LightType.Spot)
+        {
+            Debug.LogError("Spot Light Game Object is missing!");
+            enabled = false;
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Detection();
+    }
+
+    private void Detection()
+    {
+        if (spotLight == null) return;
+
+        // Kiszámítja az aktuális idõ alapján az ellipszis pozícióját
+        float angle = Time.time * orbitSpeed;
+        float x = Mathf.Cos(angle) * xAxisRadius;
+        float y = Mathf.Sin(angle) * yAxisRadius;
+
+        // Beállítja a spotlámpa helyi pozícióját az ellipszis mentén
+        spotLight.transform.localPosition = new Vector3(x, 0, y);
+
+        // A detektáló gömb középpontjának számítása:
+        // A spotlámpa pozíciójából kiindulva, a spotlámpa elõre irányában eltolva a detectionSphereDistance-el
+        gizmoSphereCenter = spotLight.transform.position + spotLight.transform.forward * detectionSphereDistance;
+        gizmoSphereRadius = detectionSphereRadius;
+
+        // OverlapSphere-rel ellenõrzi, hogy mit érint a gömb
+        // A '10' itt a maximális collider szám, amit visszaadhat
+        detectedColliders = Physics.OverlapSphere(gizmoSphereCenter, gizmoSphereRadius, hitLayers);
+
+        if (detectedColliders.Length > 0)
+        {
+            gizmoSphereDetected = true;
+            foreach (Collider col in detectedColliders)
+            {
+                //Debug.Log($"A detektáló gömb érintkezik: {col.name}");
+            }
+        }
+        else
+        {
+            gizmoSphereDetected = false;
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        if (!enabled || spotLight == null) return;
+
+        // Ellipszis rajzolása (sárga)
+        Gizmos.color = Color.yellow;
+        Vector3 previousPoint = Vector3.zero;
+        for (int i = 0; i <= 50; i++)
+        {
+            float angle = (float)i / 50f * 2f * Mathf.PI;
+            float x = Mathf.Cos(angle) * xAxisRadius;
+            float y = Mathf.Sin(angle) * yAxisRadius;
+            Vector3 currentPoint = transform.position + new Vector3(x, 0, y);
+
+            if (i > 0)
+            {
+                Gizmos.DrawLine(previousPoint, currentPoint);
+            }
+            previousPoint = currentPoint;
+        }
+        Gizmos.DrawLine(previousPoint, transform.position + new Vector3(xAxisRadius, 0, 0)); // Zárja be az ellipszist
+
+        // Detektáló gömb rajzolása
+        // Rajzoljuk ki mindig, hogy lássuk a pozícióját és méretét a szerkesztõben is
+        if (gizmoSphereDetected)
+        {
+            // Ha valamit detektált, zöld színnel rajzoljuk a gömböt
+            Gizmos.color = Color.green;
+        }
+        else
+        {
+            // Ha nem detektált semmit, piros színnel rajzoljuk a gömböt
+            Gizmos.color = Color.red;
+        }
+        // Rajzoljunk egy tömör gömböt, ha van detektálás, különben csak drótvázat
+        if (Application.isPlaying && gizmoSphereDetected)
+        {
+            Gizmos.DrawSphere(gizmoSphereCenter, gizmoSphereRadius);
+        }
+        else
+        {
+            Gizmos.DrawWireSphere(gizmoSphereCenter, gizmoSphereRadius);
+        }
+
+
+        // Ha volt detektálás, rajzoljuk ki a detektált objektumokat is
+        if (Application.isPlaying && gizmoSphereDetected && detectedColliders != null)
+        {
+            Gizmos.color = Color.blue; // Kék színnel jelöljük a detektált objektumokat
+            foreach (Collider col in detectedColliders)
+            {
+                if (col != null)
+                {
+                    Gizmos.DrawWireCube(col.bounds.center, col.bounds.size);
+                }
+            }
+        }
     }
 }
---
+++ Assets/Scripts/AssetList.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class AssetList
+{
+    /*
+     * ---
+https://assetstore.unity.com/packages/p/textures-free-122421
+https://assetstore.unity.com/packages/3d/props/exterior/rock-package-118182
+https://www.mixamo.com/
+https://free3d.com/3d-model/sword-86883.html
+https://assetstore.unity.com/packages/3d/environments/dungeons/dungeon-modular-pack-295430
+https://assetstore.unity.com/packages/3d/environments/historic/colonial-ship-70472
+https://assetstore.unity.com/packages/3d/props/rust-key-167590
+https://assetstore.unity.com/packages/3d/props/sitting-lion-statue-260994
+https://assetstore.unity.com/packages/3d/characters/humanoids/fantasy/modular-fantasy-knight-character-276754
+https://assetstore.unity.com/packages/3d/characters/animals/wolf-animated-45505
+https://assetstore.unity.com/packages/2d/textures-materials/sky/10-skyboxes-pack-day-night-32236
+https://assetstore.unity.com/packages/3d/environments/dungeons/lite-dungeon-pack-low-poly-3d-art-by-gridness-242692
+---*/
+    
+}
